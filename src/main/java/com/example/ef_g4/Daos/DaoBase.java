2376e503bad29ed658aa70bf64c89cea489bae10
dianasr <sologurenr.d@pucp.edu.pe>
2021-12-07 12:10:10 -0500

    DaoBase

---
+++ src/main/java/com/example/ef_g4/Daos/DaoBase.java
@@ -0,0 +1,24 @@
+package com.example.ef_g4.Daos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class DaoBase {
+
+    public Connection getConection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+
+        String username = "root";
+        String password = "root";
+        String database = "movies";
+        String url = "jdbc:mysql://localhost:3306/" + database;
+
+        return DriverManager.getConnection(url, username, password);
+
+    }
+}
